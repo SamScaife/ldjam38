c75adc3ab07d6eefff749ceb155dec7bac41aeac
Sam <rose@theideashouse.com>
2017-04-22 22:37:00 +0100

    Added Dialogue functionality

---
+++ Assets/Scripts/DialogueScript.js
@@ -0,0 +1,80 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+import UnityEngine.Events;
+
+var DialogueArea : GameObject;
+var Image1Area : Image; 
+var Image2Area : Image;
+var Speaker : Text;
+var Dialogue : Text;
+var Option1Button : Button;
+var Option2Button : Button;
+var Option3Button : Button;
+var Option1TextArea : Text;
+var Option2TextArea : Text;
+var Option3TextArea : Text;
+
+private var options = [Option1Button,Option2Button,Option3Button];
+
+function Start() {
+	//print('hit');
+	//start hidden
+	closeDialogueArea();
+}
+
+function UpdateDialogueArea(speakerText: String, dialogueText: String, imageAmount: int, image1 : Sprite, image2 : Sprite, optionAmount : int, option1Text: String, option2Text: String, option3Text: String, option1Event : UnityAction, option2Event : UnityAction, option3Event : UnityAction) {
+	//print('updating Dialogue area');
+	DialogueArea.SetActive(true);
+
+	Speaker.text = speakerText;
+	Dialogue.text = dialogueText;
+
+
+	//set up option1
+	Option1Button.onClick.RemoveAllListeners();
+    Option1Button.onClick.AddListener (option1Event);
+    Option1TextArea.text = option1Text;
+
+    //check if there should be an option 2
+	if(optionAmount > 1) {
+		//set up option2
+		Option2Button.onClick.RemoveAllListeners();
+	    Option2Button.onClick.AddListener (option2Event);
+	    Option2TextArea.text = option2Text;
+	} else {
+		//hide option2
+		Option2Button.gameObject.SetActive(false);
+	}
+
+	//check if there should be an option 3
+	if(optionAmount > 2) {
+		//set up option3
+		Option3Button.onClick.RemoveAllListeners();
+	    Option3Button.onClick.AddListener (option3Event);
+	    Option3TextArea.text = option3Text;
+	} else {
+		//hide option 3
+		Option3Button.gameObject.SetActive(false);
+	}
+
+	if(imageAmount > 0) {
+		Image1Area.sprite = image1;
+	} else {
+		Image1Area.gameObject.SetActive(false);
+	}
+
+	if(imageAmount > 1) {
+		Image2Area.sprite = image2;
+	} else {
+		Image2Area.gameObject.SetActive(false);
+	}
+
+}
+
+function closeDialogueArea() {
+	DialogueArea.SetActive(false);
+}
+
+
+
---
+++ Assets/Scripts/testDialogue.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var ModalScript : DialogueScript;
+
+function runTest(message : String) {
+	print('doing something');
+	var imageChoice = Resources.Load("Images/Pyro", Sprite);
+	print(imageChoice);
+	ModalScript.UpdateDialogueArea('test speaker', message, 1, imageChoice, null, 2, 'continue', 'back', '', option1, option2, null);
+}
+
+function option1() {
+	print('option 1 clicked');
+}
+
+function option2() {
+	print('option 2 clicked');
+}
+
