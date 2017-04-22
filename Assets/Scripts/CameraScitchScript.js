b97d09a408e4924f84667f36fae8582b257c6b5c
Sam <rose@theideashouse.com>
2017-04-22 15:16:09 +0100

    Added Scene switching and Camera changeing

---
+++ Assets/Scripts/CameraScitchScript.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+private var cam0 : Camera;
+private var cam1 : Camera;
+private var cam2 : Camera;
+private var cam3 : Camera;
+private var cam4 : Camera;
+
+var cameras = [cam0,cam1,cam2,cam3,cam4];
+
+function Start () {
+	for (var i = 0; i < cameras.length; i++) {
+		//turn all cameras off
+		cameras[i].enabled = false;
+	}
+	//enable the yard camera
+	cameras[0].enabled = true;
+}
+
+function Update () {
+	
+}
+
+function ChangeCamera (index : int) {
+	for (var i = 0; i < cameras.length; i++) {
+		//turn all cameras off
+		cameras[i].enabled = false;
+	}
+	//enable defined camera 
+	cameras[index].enabled = true;
+}
---
+++ Assets/Scripts/MenuScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+import UnityEngine.SceneManagement;
+
+var Destination : String;
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+ 
+function LoadScene () {
+	SceneManager.LoadScene( Destination, LoadSceneMode.Single);
+}
